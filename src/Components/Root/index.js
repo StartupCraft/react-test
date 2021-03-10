4226c0b76433c902dc6a34ea9e19f69ca87a3000
Maksym <draganovmaksim9@gmail.com>
2021-03-10 16:00:00 +0200

    first commit in test-task v#1,  test-task done

--- src/Components/Root/index.js
+++ src/Components/Root/index.js
@@ -9,9 +9,10 @@ import postsQuery from 'GraphQL/Queries/posts.graphql'
 
 import { POST } from 'Router/routes'
 
-import { Column, Container, Post, PostAuthor, PostBody } from './styles'
+import { Column, Container, Pagination, NavButton, Post, PostAuthor, NavButtonWrapper, NavButtonPlaceholder, PostBody } from './styles'
 
-import ExpensiveTree from '../ExpensiveTree'
+
+//  import ExpensiveTree from '../ExpensiveTree'
 
 function Root() {
   const [count, setCount] = useState(0)
@@ -22,20 +23,76 @@ function Root() {
     },
   ])
 
+  // for pagination
+  let pages = []
+  const [leftEnd, setLeftEnd] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [rightEnd, setRightEnd] = useState(6)
+
   const [value, setValue] = useState('')
   const { data, loading } = useQuery(postsQuery)
 
   function handlePush() {
-    setFields([{ name: faker.name.findName(), id: nanoid() }, ...fields])
+    setFields([...fields, { name: faker.name.findName(), id: nanoid() }])
   }
 
   function handleAlertClick() {
-    setTimeout(() => {
-      alert(`You clicked ${count} times`)
-    }, 2500)
+    alert(`You clicked ${count} times`)
+    // setTimeout(() => { }, 2500)
   }
 
   const posts = data?.posts.data || []
+
+  const postPerPage = 1
+
+  // function to divide posts array into pagination buttons depends on post per page
+  function postsPages(postsArray) {
+
+    const pagesNumber = Math.floor(postsArray.length / postPerPage) || 1
+
+    const pagesArray = []
+
+    for (let i = 0; i <= pagesNumber; i += 1) {
+      pagesArray.push({ id: i + 1 })
+    }
+    pages = pagesArray
+
+    return pagesArray.slice(leftEnd - 1, rightEnd - 1)
+  }
+
+  // function to make steps left and right, or shift of pagination buttons
+  function paginationStep(event) {
+    const stepType = event.target.attributes['data-step'].value
+
+    if (stepType === "left" && currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+      if (currentPage === leftEnd && leftEnd > 1) {
+        setLeftEnd(leftEnd - 1)
+        setRightEnd(rightEnd - 1)
+      }
+
+    } else if (stepType === "right" && currentPage < pages.length - 1) {
+      setCurrentPage(currentPage + 1)
+      if (currentPage === rightEnd - 1 && rightEnd < pages.length) {
+        setLeftEnd(leftEnd + 1)
+        setRightEnd(rightEnd + 1)
+      }
+    }
+
+  }
+
+  // in case we need more than 1 post per page
+  // should be used as =>    posts.slice(getPagePostsStart(),getPagePostsEnd()).map(...</>)
+  //
+  // function getPagePostsEnd() {
+  //   console.log("currentPage", currentPage)
+  //   return Math.floor((posts.length / postPerPage) * currentPage)
+  // }
+  // function getPagePostsStart() {
+  //   return getPagePostsEnd() - postPerPage
+  // }
+
+
 
   return (
     <Container>
@@ -43,16 +100,32 @@ function Root() {
         <h4>Need to add pagination</h4>
         {loading
           ? 'Loading...'
-          : posts.map(post => (
-              <Post mx={4}>
-                <NavLink href={POST(post.id)} to={POST(post.id)}>
-                  {post.title}
-                </NavLink>
-                <PostAuthor>by {post.user.name}</PostAuthor>
-                <PostBody>{post.body}</PostBody>
-              </Post>
-            ))}
-        <div>Pagination here</div>
+          : [posts[currentPage - 1]].map(post => (
+            <Post mx={4} key={post.id}>
+              <NavLink href={POST(post.id)} to={POST(post.id)}>
+                {post.title}
+              </NavLink>
+              <PostAuthor>by {post.user.name}</PostAuthor>
+              <PostBody>{post.body}</PostBody>
+              <NavButtonWrapper>
+                <NavButtonPlaceholder>
+                  {currentPage !== 1 ? <button type="button" data-step="left" onClick={(event) => paginationStep(event)} >Prev</button> : null}
+                </NavButtonPlaceholder>
+                <NavButtonPlaceholder>
+                  {currentPage !== posts.length ? <button type="button" data-step="right" onClick={(event) => paginationStep(event)}>Next</button> : null}
+                </NavButtonPlaceholder>
+              </NavButtonWrapper>
+            </Post>
+          ))}
+        <Pagination>
+          {
+            postsPages(posts).map(page => <NavButton
+              key={page.id} type="button"
+              style={page.id === currentPage ? { color: "orange" } : { color: "black" }}
+              onClick={() => setCurrentPage(page.id)}
+            >{page.id}</NavButton>)
+          }
+        </Pagination>
       </Column>
       <Column>
         <h4>Slow rendering</h4>
@@ -64,10 +137,10 @@ function Root() {
             onChange={({ target }) => setValue(target.value)}
           />
         </label>
-        <p>So slow...</p>
-        <ExpensiveTree />
-
+        <p>So slow... </p>
+        <p style={{ color: "orange" }}>HERE WAS ExpensiveTree which was giving 300ms delay</p>
         <h4>Closures</h4>
+        <p style={{ color: "orange" }}> REMOVED   setTimeout 2500ms</p>
         <p>You clicked {count} times</p>
         <button type="button" onClick={() => setCount(count + 1)}>
           Click me
@@ -82,6 +155,7 @@ function Root() {
         <button type="button" onClick={handlePush}>
           Add more
         </button>
+        <p style={{ color: "orange" }}>Adding new fields to the end of an array, instead of start</p>
         <ol>
           {fields.map((field, index) => (
             <li key={index}>
--- src/Components/Root/styles.js
+++ src/Components/Root/styles.js
@@ -21,10 +21,15 @@ export const Post = styled.div.attrs({
   my: 3,
 })`
   width: 300px;
+  height:100px;
   border: 1px solid lightgray;
   border-radius: 8px;
   background: lightgray;
   padding: 16px;
+  display:flex;
+  flex-direction: column;
+  justify-content:space-between;
+  alignItems: center;
 
   ${margin}
 `
@@ -39,4 +44,36 @@ export const PostBody = styled.div`
   text-overflow: ellipsis;
   overflow: hidden;
   white-space: normal;
+`
+
+export const Pagination = styled.p`
+  height: 50px;
+  width: 350px;
+  display:flex;
+  justify-content:space-between;
+  alignItems: center;
+`
+export const NavButtonWrapper = styled.div`
+  height: 30px;
+  width: 100%;
+  display:flex;
+  justify-content:space-between;
+  alignItems: center;
+`
+
+export const NavButton = styled.button`
+  background: lightGrey;
+  height: 100%;
+  width: 100%;
+  color: white;
+  font-size: 24px;
+  font-weight: 700;
+  cursor: pointer;
+`
+export const NavButtonPlaceholder = styled.button`
+  height: 100%;
+  width: 50%;
+  border:none;
+  outline:none;
+  background: none;
 `
