8ac1322a965ad12ddbb35c6e767eed536dac2d9c
Joseph Paolinelli <jdpniko@undiscoveredtech.com>
2022-06-21 20:20:34 -0700

    Fixed ExpensiveTree's slow rendering

--- src/Components/ExpensiveTree.js
+++ src/Components/ExpensiveTree.js
@@ -4,8 +4,10 @@ function ExpensiveTree() {
   const now = performance.now()
   while (performance.now() - now < 100) {
     // Emulate some expensive calculations which takes 300ms
+    // JP - 06-21-22: Instead of calculations, emulating a wait() function to create exactly 300ms delay which showcases the lag perfectly
+    setTimeout(() => { }, 300);
   }
   return <div />
 }
-
-export default ExpensiveTree
+// JP - 06-21-22: Wrappin the ExpensiveTree using React Memorize, to prevent unnecessary rendering which solves the slowdown
+export default React.memo(ExpensiveTree)
